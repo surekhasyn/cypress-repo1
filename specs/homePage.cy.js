3837e7ea0588ca14dd35311a762bbe50bac0a4d6
surekhasyn <surekhapanda74@gmail.com>
2024-05-13 13:27:54 +0530

    Update homePage.cy.js

--- specs/homePage.cy.js
+++ specs/homePage.cy.js
@@ -5,11 +5,8 @@ describe('Home Page', () => {
     homePage.goto()
   })
 
-  it('should find an older post by loading more', () => {
-    const postTitle = 'Cypress.io Review'
-    homePage.findPostByLoadingMore(postTitle)
-
-    homePage.postTitleExists(postTitle).should('be.visible')
+  it('should display 7 posts per page', async () => {
+    homePage.posts().should('have.length', 7)
   })
 
   it('should display 7 posts per page', async () => {
